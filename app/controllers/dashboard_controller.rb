5844e1602a695eac2a17247b529a94ae3ee1343e
geo_rioz <91685749+Geo52@users.noreply.github.com>
2025-04-13 22:21:33 +0000

    test to see if the plaid api works in production

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,12 @@
 class DashboardController < ApplicationController
   def index
-    
+      request = Plaid::InstitutionsGetRequest.new({
+      count: 5,
+      offset: 1,
+      country_codes: [ "US" ]
+      })
+
+      response = PlaidClient.institutions_get(request)
+      @banks = response.institutions.first.name
   end
 end
