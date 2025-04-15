b033744a25fc4de18f22da90244816c0032cff26
Ricardo Perez <perez.ricardo.p1@gmail.com>
2025-04-15 01:07:47 +0000

    Added first test for monthly expenses

---
+++ spec/requests/plaid_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+# spec/requests/plaid_spec.rb
+RSpec.describe "Plaid Monthly Expenses", type: :request do
+  describe "GET /plaid/monthly_expenses" do
+    it "returns an error if no access token is present" do
+      # Makes a GET request without setting up the session
+      get "/plaid/monthly_expenses"
+
+      # Expects the controller to respond with 400 Bad Request since token is missing
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+end
